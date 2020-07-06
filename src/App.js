2912b738b0d71f300f597d35f624186bdd9cd90c
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2020-07-06 12:20:30 -0600

    Convert App.js to function based component with hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>Hi. I'm a react app</h1>
-				<p>This is really working!</p>
-				<Person />
-			</div>
-		);
-	}
-}
+const App = props => {
+	const [ personsState, setPersonsState ] = useState( {
+		persons: [
+			{ name: 'Bradley', age: 34 },
+			{ name: 'Jessica', age: 34 },
+			{ name: 'Bob', age: 54 }
+		]
+	} );
+
+	const [ otherState, setOtherState ] = useState( 'Some other value' );
+	
+	console.log( personsState, otherState );
+
+	const changeNameHandler = () => {
+	// console.log('Was clicked!');
+	// Don't do this: this.state.persons[0].name = 'Taco';
+		setPersonsState( {
+			persons: [
+				{ name: 'Taco', age: 34 },
+				{ name: 'Jessica', age: 34 },
+				{ name: 'Bob', age: 20 }
+			]
+		} );
+	};
+
+	return (
+		<div className="App">
+			<h1>Hi. I'm a react app</h1>
+			<p>This is really working!</p>
+			<button onClick={ changeNameHandler }>Change Name</button>
+			<Person name={ personsState.persons[0].name } age= { personsState.persons[0].age }>
+					My Hobbies: Gaming
+			</Person>
+			<Person name={ personsState.persons[1].name } age= { personsState.persons[1].age } />
+			<Person name={ personsState.persons[2].name } age= { personsState.persons[2].age } />
+		</div>
+	);
+};
 
 export default App;
