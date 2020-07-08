85aee5d865e8b214aa6bb0d021a0681c21295943
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2020-07-07 19:28:34 -0600

    Remove styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
 
 const StyledButton = styled.button`
 	background-color: ${ props => props.alt ? 'red' : 'green' };
@@ -82,11 +81,11 @@ class App extends Component {
 				<div className="App">
 					<h1>Hi, I'm a React App</h1>
 					<p className={ classes.join( ' ' ) }>This is really working!</p>
-					<StyledButton
+					<button
 						alt={ this.state.showPersons }
 						onClick={ this.togglePersonsHandler }>
 							Toggle Persons
-					</StyledButton>
+					</button>
 					{ persons }
 				</div>
 		);
